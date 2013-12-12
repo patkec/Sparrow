c05faf92ea610d63d4a1e155749a5721f919deda
Simon Stamcar <patko@outlook.com>
2013-12-12 17:48:32 +0100

    Totals are also included with the model.

--- src/Sparrow.Web/Models/Drafts/DraftDetailViewModel.cs
+++ src/Sparrow.Web/Models/Drafts/DraftDetailViewModel.cs
@@ -9,6 +9,9 @@ namespace Sparrow.Web.Models.Drafts
         public Guid Id { get; set; }
         public string Title { get; set; }
         public double Discount { get; set; }
+        public decimal DiscountAmount { get; set; }
+        public decimal Total { get; set; }
+        public decimal Subtotal { get; set; }
         public DateTime CreatedOn { get; set; }
         public string OwnerFullName { get; set; }
         public CustomerViewModel Customer { get; set; }
