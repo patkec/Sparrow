564fba15880693ca7f7ba88d9f9c05a1624cf24d
Simon Stamcar <patko@outlook.com>
2013-12-29 23:58:52 +0100

    Added claims auth support

---
+++ src/Sparrow.Web/Security/ClaimsTransformationOptions.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace Sparrow.Web.Security
+{
+    public class ClaimsTransformationOptions
+    {
+        public ClaimsAuthenticationManager ClaimsAuthenticationManager { get; set; }
+    }
+}
